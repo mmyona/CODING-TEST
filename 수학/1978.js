72325b7ce7ed20358b0303f484f9574c177f38b5
mmyona <hyon2001@ajou.ac.kr>
2022-02-02 02:21:36 +0900

    알고리즘_구현

--- 수학/1978.js
+++ 수학/1978.js
@@ -1,11 +1,11 @@
-let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+let input = require('fs').readFileSync('question.txt').toString().split('\n');
 
 let num = Number(input[0]); //수의 개수
 let arr = input[1].split(' ').map(x => Number(x)); //수
-const result=[];
+let result=0; //소수의 개수
 
 function test(number){
-    for(let i=number;i<number;i--){
+    for(let i=number-1;i>1;i--){
         if(number%i===0){
             return 0;
         }
@@ -13,9 +13,9 @@ function test(number){
     return 1;
 }
 
-for(let i=0;i<=num;i++){
-    if(test(arr[i])){
-        result.push(arr[i]);
+for(let i=0;i<num;i++){
+    if(arr[i]>1&&test(arr[i])){
+        result++;
     }
 }
 
