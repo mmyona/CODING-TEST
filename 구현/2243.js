af89fd09eac1ce60e71de632207436b25d09907b
mmyona <hyon2001@ajou.ac.kr>
2022-02-05 04:59:54 +0900

    별찍기 도장 깨기 ~ing

--- 구현/2243.js
+++ 구현/2243.js
@@ -3,11 +3,11 @@ let result='*';
 let empty='';
 
 for(let i=1;i<Number(input);i++){
-    empty+=' '; //맨 처음 줄의 공백
+    result+='**'; //맨 처음 줄의 *
 }
 
 for(let i=Number(input);i>0;i--){
     console.log(empty+result);
-    result+='**' //*을 하나씩 늘려 나감
-    empty=empty.slice(1); //공백을 하나씩 줄여 나감
+    result=result.slice(2) //*을 두개씩 줄여 나감
+    empty+=' '; //공백을 하나씩 늘려 나감
 }
---
+++ 구현/2245.js
@@ -0,0 +1,2 @@
+let input = require('fs').readFileSync('question.txt').toString();
+
