f4401d9c61f8328d0e83c1dc3fce046870e7b968
mmyona <hyon2001@ajou.ac.kr>
2022-02-07 23:59:15 +0900

    11721번 이상함..

--- 11721.js
+++ 11721.js
@@ -1,6 +1,6 @@
 let input = require('fs').readFileSync('question.txt').toString();
 
-let length=input.length;
+/*let length=input.length;
 let result=[]; 
 let i=0;
 let left=''; //남은 글자들(10개 미만)
@@ -9,11 +9,17 @@ for(j=0;j+10<=length;j+=10){ //남은 글자 개수가 10개 미만이면 for문
     result[i]=input.substr(j,10);
     i++;
     left=input.substr(j+10);
+    console.log(left);
 }
 
-for(let j=0;j<i;j++){
-    console.log(result[j]);
+for(let k=0;k<i;k++){
+    console.log(result[k]);
 }
-if(left!==''){
+if(left.length<10){
     console.log(left); 
+}*/
+
+while(input.length) {
+    console.log(input.slice(0, 10));
+    input = input.slice(10);
 }
