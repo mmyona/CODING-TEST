d8c92a3e17f487dd4ebf11d38d70be70f44eb1f9
mmyona <hyon2001@ajou.ac.kr>
2022-03-09 23:10:39 +0900

    2022.03.09

--- 문자열/1181.js
+++ 문자열/1181.js
@@ -3,6 +3,20 @@ let input = require('fs').readFileSync('question.txt').toString().trim().split('
 let set=new Set(input.slice(1));
 let newArr=[...set]; 
 //중복을 없애기
+/*
+const set = new Set(dupArr);
+
+중복값이 있는 배열을 Set 객체로 만들어서 중복을 제거한 후,
+
+const uniqueArr = [...set];
+
+Spread Operator(전개연산자)를 사용하여 Set 객체를 다시 배열로 변환하였습니다.
+
+Set 객체를 배열로 변환할 때, Spread Operator 대신
+
+Array.from() 또는 forEach() 문을 사용할 수도 있습니다.
+
+*/
 
 newArr.sort(); //알파벳 순서로 정렬
 
